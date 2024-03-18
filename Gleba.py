e8e9828dae91cf7cbd38f6878d6242dcbda71dd4
Isr4elEly <israelely76@gmail.com>
2024-03-18 01:19:35 -0300

    atualização das bases SIGEF

--- Gleba.py
+++ Gleba.py
@@ -1,4 +1,4 @@
-%% 
+# %% 
 from dataclasses import dataclass
 import geopandas as gpd
 import pandas as pd
@@ -13,3 +13,27 @@ pd.set_option('display.precision', 4)
 #area_estudo = gpd.read_file('../drive/shapes_transformados/amazonia-legal-com-maranhao.shp')
 #uf = gpd.read_file('../drive/uf.shp')
 #sr_ponto = gpd.read_file("../drive/Shapes Bases de Glebas e Projetos data 01.03.23/sr_incra.shp")
+
+# %%
+
+# Tratar a camada SIGEF Geral
+
+sigef_geral = gpd.read_file('../certificados_04mar2024.gpkg', layer='sigef_geral')
+snci_geral = gpd.read_file('../certificados_04mar2024.gpkg', layer='snci_geral')
+
+sigef_geral.to_file('../glebas-federais.gpkg', layer='sigef-particular')
+snci_geral.to_file('../glebas-federais.gpkg', layer='snci-particular')
+# %%
+estados = {
+    'RO': 'Rondônia', 'AC': 'Acre', 
+    'AM': 'Amazonas', 'RR': 'Roraima', 
+    'PA': 'Pará', 'AP': 'Amapá', 'TO': 'Tocantins', 
+    'MA': 'Maranhão', 'MT': 'Mato Grosso'}
+# %%
+
+for key, value in estados.items():
+    sigef_geral[sigef_geral['uf_sigla']==key].to_file('../glebas-federais.gpkg', layer=f'sigef-{value}')
+
+for key, value in estados.items():
+    snci_geral[snci_geral['uf_municip']==key].to_file('../glebas-federais.gpkg', layer=f'snci-{value}')
+# %%
